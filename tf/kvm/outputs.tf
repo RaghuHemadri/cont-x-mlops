75680f66c15f0bd5d3dec21d5db1dc6429ada90a
RaghuHemadri <45795135+RaghuHemadri@users.noreply.github.com>
2025-05-08 23:21:38 -0400

    Update outputs.tf

--- tf/kvm/outputs.tf
+++ tf/kvm/outputs.tf
@@ -1,5 +1,7 @@
-output "floating_ip_out" {
-  description = "Floating IP assigned to node1"
-  value       = openstack_networking_floatingip_v2.floating_ip.address
+output "controller_floating_ip" {
+  value = openstack_networking_floatingip_v2.controller_fip.address
 }
 
+output "worker_ips" {
+  value = [for instance in openstack_compute_instance_v2.worker : instance.access_ip_v4]
+}
